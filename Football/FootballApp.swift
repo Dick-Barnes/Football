0e514db781cde380786836f7affc30e6984121a3
Dick Barnes <rbarnes62@comcast.net>
2025-06-28 15:59:50 -0400

    Initial Commit

---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by Richard Barnes on 6/28/25.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
